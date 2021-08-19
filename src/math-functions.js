5b78f8d10a377c8fa1ef3438f742c00859411be5
Julius Franklin <jjfranklin88@outlook.com>
2021-08-19 16:16:42 -0400

    sum function commit

--- src/math-functions.js
+++ src/math-functions.js
@@ -3,19 +3,24 @@
 /////////////////////////////////////
 /* Problem 1
 
-Write a function called sum() that takes in two numbers as arguments and 
-then returns an array where the first element is the sum of those numbers, 
-and the second element is a concatenated string that EXACTLY follows this 
+Write a function called sum() that takes in two numbers as arguments and
+then returns an array where the first element is the sum of those numbers,
+and the second element is a concatenated string that EXACTLY follows this
 example and uses the values that were input into the function:
 
 "The sum of 4 and 7 is 11."
 */
 
 export function sum(a, b) {
+    let sum = 4 + 7;
+    let sumResult = [];
+    sumResult.push(sum);
+    sumResult.push(`The sum of ${a} and ${b} is ${sum}.`);
+    return sumResult;
 
 }
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 // Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
@@ -29,24 +34,24 @@ export function multiply(a, b) {
 
 }
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 // Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiplyThreeNumbers() that takes in three 
-numbers as separate arguments and returns 
-an array where the first element is the sum of those three numbers, 
-the second element is the product of those three numbers,  
-and the third and fourth elements are strings that EXACTLY follow this example and 
+Write a function called sumAndMultiplyThreeNumbers() that takes in three
+numbers as separate arguments and returns
+an array where the first element is the sum of those three numbers,
+the second element is the product of those three numbers,
+and the third and fourth elements are strings that EXACTLY follow this example and
 use the values that were input into the function:
 
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
 
-IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. 
-To do addition, use your sum() function, and to do multiplication, use your multiply() 
-function that you've already created. You're going to have to be resourceful to figure out 
+IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function.
+To do addition, use your sum() function, and to do multiplication, use your multiply()
+function that you've already created. You're going to have to be resourceful to figure out
 how to do this. However, you may continue to use the + operator for string concatenation.
 */
 
@@ -54,21 +59,21 @@ export function sumAndMultiplyThreeNumbers(a, b, c) { //eslint-disable-line
 
 }
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 // Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArrayWithThreeNumbers() that takes in an array of numbers 
-as its single argument and then returns an array where the first element is the sum 
-of the numbers in the array, and the second element is a string that EXACTLY follows 
+Write a function called sumArrayWithThreeNumbers() that takes in an array of numbers
+as its single argument and then returns an array where the first element is the sum
+of the numbers in the array, and the second element is a string that EXACTLY follows
 this example and uses the values that were input into the function:
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. 
-To do addition, use your sum() function that you've already created. You're going to 
-have to be resourceful to figure out how to do this. However, you may continue 
+IMPORTANT DETAIL: You may not use the arithmetic operator + in this function.
+To do addition, use your sum() function that you've already created. You're going to
+have to be resourceful to figure out how to do this. However, you may continue
 to use the + operator for string concatenation.
 */
 
@@ -76,22 +81,22 @@ export function sumArrayWithThreeNumbers(sumArr) {
 
 }
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 // Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 5
-Write a function called multiplyArrayWithThreeNumbers() that takes an array of numbers 
-as its argument and returns an array whose first element is the product of those numbers, 
-and the second element is a string that EXACTLY follows this example and uses the values 
+Write a function called multiplyArrayWithThreeNumbers() that takes an array of numbers
+as its argument and returns an array whose first element is the product of those numbers,
+and the second element is a string that EXACTLY follows this example and uses the values
 that were input into the function:
 
 "The numbers 2,3,4 have a product of 24."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. 
-To do multiplication, use your multiply() function that you've already created. 
-You're going to have to be resourceful to figure out how to do this. 
-This function should handle an array containing three elements. However, 
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function.
+To do multiplication, use your multiply() function that you've already created.
+You're going to have to be resourceful to figure out how to do this.
+This function should handle an array containing three elements. However,
 you may continue to use the + operator for string concatenation.
 */
 
@@ -99,7 +104,7 @@ export function multiplyArrayWithThreeNumbers(multArr) { //eslint-disable-line
 
 }
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 
@@ -120,5 +125,5 @@ export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
 }
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 // You're done! Submit the link to the repo following the instructions in Canvas.
